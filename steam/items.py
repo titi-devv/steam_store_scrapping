68bff177fae4885ee69d636ec2e14164da1041af
thibaut lehmann <thiblehm12@gmail.com>
2023-08-11 11:59:37 +0200

    data processing done

--- steam/items.py
+++ steam/items.py
@@ -1,19 +1,94 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
+from w3lib.html import remove_tags
 import scrapy
+from scrapy.loader.processors import TakeFirst, MapCompose, Join
+
+
+def remove_html(review_summary):
+    cleaned_review_summary = ''
+    try:
+        cleaned_review_summary = remove_tags(review_summary)
+    except TypeError:
+        cleaned_review_summary = 'No reviews'
+    return cleaned_review_summary
+
+
+def clean_discount_rate(discount_rate):
+    if discount_rate:
+        return discount_rate.lstrip('-')
+    return discount_rate
+
+
+def get_original_price(html_markup):
+    original_price = ''
+    selector_obj = scrapy.Selector(text=html_markup)
+    div_with_discount = selector_obj.xpath(
+        ".//div[contains(@class,'discount_original_price')]/text()").get()
+    if div_with_discount:
+        original_price = div_with_discount
+    else:
+        original_price = selector_obj.xpath(
+            ".//div[contains(@class,'discount_final_price')]/text()").get()
+    return original_price
+
+
+def get_discounted_price(html_markup):
+    original_price = ''
+    selector_obj = scrapy.Selector(text=html_markup)
+    div_with_discount = selector_obj.xpath(
+        ".//div[contains(@class,'discount_original_price')]/text()").get()
+    if div_with_discount:
+        original_price = selector_obj.xpath(
+            ".//div[contains(@class,'discount_final_price')]/text()").get()
+    else:
+        original_price = None
+    return original_price
+
+
+def get_platforms(one_class):
+    platforms = []
+
+    platform = one_class.split(' ')[-1]
+    if platform == 'win':
+        platforms.append('Windows')
+    if platform == 'mac':
+        platforms.append('Mac os')
+    if platform == 'lin':
+        platforms.append('Linux')
+    if platform == 'vr_supported':
+        platforms.append('VR Supported')
+    return platforms
 
 
 class SteamItem(scrapy.Item):
+    game_url = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+    img_url = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+    game_name = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+    release_date = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+    platforms = scrapy.Field(
+        input_processor=MapCompose(get_platforms)
+    )
+    reviews_summary = scrapy.Field(
+        input_processor=MapCompose(remove_html),
+        output_processor=TakeFirst()
+    )
 
-    game_url = scrapy.Field()
-    img_url = scrapy.Field()
-    game_name = scrapy.Field()
-    release_date = scrapy.Field()
-    platforms = scrapy.Field()
-    reviews_summary = scrapy.Field()
-    original_price = scrapy.Field()
-    discounted_price = scrapy.Field()
-    discount_rate = scrapy.Field()
+    original_price = scrapy.Field(
+        input_processor=MapCompose(get_original_price),
+        output_processor=TakeFirst()
+    )
+    discounted_price = scrapy.Field(
+        input_processor=MapCompose(get_discounted_price),
+        output_processor=TakeFirst()
+    )
+    discount_rate = scrapy.Field(
+        input_processor=MapCompose(clean_discount_rate),
+        output_processor=TakeFirst()
+    )
--- steam/spiders/best_selling.py
+++ steam/spiders/best_selling.py
@@ -1,7 +1,7 @@
 import json
 import scrapy
 from ..items import SteamItem
-from w3lib.html import remove_tags
+
 from scrapy.loader import ItemLoader
 
 
@@ -22,55 +22,6 @@ class BestSellingSpider(scrapy.Spider):
                 'start': 0
             }
         )
-
-    def get_platforms(self, list_classes):
-        platforms = []
-        for item in list_classes:
-            platform = item.split(' ')[-1]
-            if platform == 'win':
-                platforms.append('Windows')
-            if platform == 'mac':
-                platforms.append('Mac os')
-            if platform == 'lin':
-                platforms.append('Linux')
-            if platform == 'vr_supported':
-                platforms.append('VR Supported')
-        return platforms
-
-    def remove_html(self, review_summary):
-        cleaned_review_summary = ''
-        try:
-            cleaned_review_summary = remove_tags(review_summary)
-        except TypeError:
-            cleaned_review_summary = 'No reviews'
-        return cleaned_review_summary
-
-    def clean_discount_rate(self, discount_rate):
-        if discount_rate:
-            return discount_rate.lstrip('-')
-        return discount_rate
-
-    def get_original_price(self, selector_obj):
-        original_price = ''
-        div_with_discount = selector_obj.xpath(
-            ".//div[contains(@class,'discount_original_price')]/text()").get()
-        if div_with_discount:
-            original_price = div_with_discount
-        else:
-            original_price = selector_obj.xpath(
-                "normalize-space(.//div[contains(@class,'discount_final_price')]/text())").get()
-        return original_price
-
-    def get_discounted_price(self, selector_obj):
-        original_price = ''
-        div_with_discount = selector_obj.xpath(
-            ".//div[contains(@class,'discount_original_price')]/text()").get()
-        if div_with_discount:
-            original_price = selector_obj.xpath(
-                ".//div[contains(@class,'discount_final_price')]/text()").get()
-        else:
-            original_price = None
-        return original_price
 
     def parse(self, response):
         resp = json.loads(response.body)
